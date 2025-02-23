5f93175a0e1c1af495be25a4113460f3ca270783
Hemanth Krishna A B <hemanthkrishnabiju22@gmail.com>
2025-02-23 21:46:13 +0530

    Delete server directory

--- server/models/Session.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const sessionSchema = new mongoose.Schema({
-  sessionCode: { type: String, unique: true, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-const Session = mongoose.model('Session', sessionSchema);
-module.exports = Session;
