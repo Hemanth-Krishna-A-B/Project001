755b65bcbc4af4a31a3dbaf4ed250cd7b00c572a
Hemanth Krishna A B <hemanthkrishnabiju22@gmail.com>
2025-02-23 21:38:13 +0530

    Delete webapp directory

--- webapp/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // You can add your global styles here
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-);
